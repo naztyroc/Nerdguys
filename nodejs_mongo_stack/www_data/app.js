8770d94abc719af740475bf86eca740cfe181226
Rashian Burns <r.burns@celmatix.com>
2018-12-18 17:38:01 -0500

    removed nodejs .deb installation and moved to nvm deployment. Also addded mongoDB driver for nodejs with test js script update

--- nodejs_mongo_stack/www_data/app.js
+++ nodejs_mongo_stack/www_data/app.js
@@ -1,3 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+var database = undefined;
+var dbUrl = 'mongodb://db:27017/security';
+MongoClient.connect(dbUrl, function(err, db) {
+  if (err) {
+    throw err;
+  } else {
+    database = db;
+    console.log('MongoDB connection successful');
+}
+});
+
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
